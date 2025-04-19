7242e376cf3b42452e79be72ae37fcf55858f7b6
mohammedfarhan99 <farhan@LAPTOP-TABKC6QG>
2025-04-20 00:56:50 +0530

    2: Added theme changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
-import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://mohammedfarhan99.github.io/',
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output:'server',
+  integrations: [mdx(), sitemap(), tailwind(), icon()],
+  adapter: netlify(),
 });
